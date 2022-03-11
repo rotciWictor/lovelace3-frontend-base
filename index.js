2c5cdfa44889831b575b45d5bd3f328780118a47
Victor Campos <victorhscampos@gmail.com>
2022-03-11 15:10:06 -0300

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+
 app.use('/', express.static('src/home'));
 app.use('/aboutme', express.static('src/aboutme'));
 
--- src/aboutme/script.js
+++ src/aboutme/script.js
@@ -1,1 +1,17 @@
-const $ = require( "jquery" )( window );
+req()
+function req() {
+    fetch("https://simple-market-backend.herokuapp.com/aboutme")
+        .then(response => response.json())
+        .then(data => {
+            write(data)
+        })
+}
+
+function write(data) {
+    $("#greeting").text(data.greeting)
+    $("#aboutMe").text(data.aboutMe)
+    $("#email").text(data.email)
+    $("#linkedin").text(data.linkedin)
+    $("#mobileNumber").text(data.mobileNumber)
+    $("#reminder").text(data.reminder)
+}
